2a2ab838c87972479eb750a3418b1eee1e2d13c1
Chimaobi Marcellinus <chimaobimarcellinus@gmail.com>
2024-05-08 20:51:34 +0100

    eliminating heroku syntax for vercel

--- practice/settings.py
+++ practice/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,4 +130,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
