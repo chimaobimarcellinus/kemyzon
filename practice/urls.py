a8d2fd1f9fd1e29aba38f1205683021327860380
Chimaobi Marcellinus <chimaobimarcellinus@gmail.com>
2024-05-08 22:10:09 +0100

    static commit

--- practice/urls.py
+++ practice/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     
@@ -27,4 +29,4 @@ urlpatterns = [
     path('kemyzon/', include('kemyzon.urls'))
 
 
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT )
